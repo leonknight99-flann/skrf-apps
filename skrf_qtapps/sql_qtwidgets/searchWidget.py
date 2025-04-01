d72fb4cd641c3c3b32fea2b08b03336d1bd88d27
leonknight99-flann <leon.knight@flann.com>
2025-04-01 17:32:34 +0100

    Adding a serial number selection. Not yet filtering

--- skrf_qtapps/sql_qtwidgets/searchWidget.py
+++ skrf_qtapps/sql_qtwidgets/searchWidget.py
@@ -17,37 +17,40 @@ class SQLDataSearchWidget(QtWidgets.QWidget):
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
 
         self.plotspec_button = QtWidgets.QPushButton("Plot Spec")
-        self.label_instNum = QtWidgets.QLabel("Input Instrument Number", self)
         self.lineEdit_instNum = QtWidgets.QLineEdit(self)
-        self.label_partID = QtWidgets.QLabel("Input Part ID", self)
         self.lineEdit_partID = QtWidgets.QLineEdit(self)
-        self.label_filter = QtWidgets.QLabel("Filter File List", self)
         self.lineEdit_filter = QtWidgets.QLineEdit(self)
         self.plot_button = QtWidgets.QPushButton("Plot")
         self.hlayout_getScan = QtWidgets.QHBoxLayout()
         self.hlayout_getScan.addWidget(self.plotspec_button)
-        self.hlayout_getScan.addWidget(self.label_instNum)
+        self.hlayout_getScan.addWidget(QtWidgets.QLabel("Input Instrument Number"))
         self.hlayout_getScan.addWidget(self.lineEdit_instNum)
-        self.hlayout_getScan.addWidget(self.label_partID)
+        self.hlayout_getScan.addWidget(QtWidgets.QLabel("Input Part ID"))
         self.hlayout_getScan.addWidget(self.lineEdit_partID)
-        self.hlayout_getScan.addWidget(self.label_filter)
+        self.hlayout_getScan.addWidget(QtWidgets.QLabel("Filter File List"))
         self.hlayout_getScan.addWidget(self.lineEdit_filter)
         self.hlayout_getScan.addWidget(self.plot_button)
 
         self.hlayout_lists = QtWidgets.QHBoxLayout()
 
-        self.listWidget_PartIDs = QtWidgets.QListWidget(self)
-        self.listWidget_PartIDs.setSelectionMode(QtWidgets.QListWidget.ExtendedSelection)
-        self.scrollBar_PartIDs = QtWidgets.QScrollBar(self)
-        self.listWidget_PartIDs.setVerticalScrollBar(self.scrollBar_PartIDs)
+        self.listWidget_partIDs = QtWidgets.QListWidget(self)
+        self.listWidget_partIDs.setSelectionMode(QtWidgets.QListWidget.ExtendedSelection)
+        self.scrollBar_partIDs = QtWidgets.QScrollBar(self)
+        self.listWidget_partIDs.setVerticalScrollBar(self.scrollBar_partIDs)
 
-        self.listWidget_SerialNums = QtWidgets.QListWidget(self)
-        self.listWidget_SerialNums.setSelectionMode(QtWidgets.QListWidget.ExtendedSelection)
-        self.scrollBar_SerialNums = QtWidgets.QScrollBar(self)
-        self.listWidget_SerialNums.setVerticalScrollBar(self.scrollBar_SerialNums)
+        self.listWidget_serialNums = QtWidgets.QListWidget(self)
+        self.listWidget_serialNums.setSelectionMode(QtWidgets.QListWidget.ExtendedSelection)
+        self.scrollBar_serialNums = QtWidgets.QScrollBar(self)
+        self.listWidget_serialNums.setVerticalScrollBar(self.scrollBar_serialNums)
 
-        self.hlayout_lists.addWidget(self.listWidget_PartIDs)
-        self.hlayout_lists.addWidget(self.listWidget_SerialNums)
+        self.listWidget_dataFiles = QtWidgets.QListWidget(self)
+        self.listWidget_dataFiles.setSelectionMode(QtWidgets.QListWidget.ExtendedSelection)
+        self.scrollBar_dataFiles = QtWidgets.QScrollBar(self)
+        self.listWidget_dataFiles.setVerticalScrollBar(self.scrollBar_dataFiles)
+
+        self.hlayout_lists.addWidget(self.listWidget_partIDs)
+        self.hlayout_lists.addWidget(self.listWidget_serialNums)
+        self.hlayout_lists.addWidget(self.listWidget_dataFiles)
 
         self.verticalLayout.addLayout(self.hlayout_getScan)
         self.verticalLayout.addLayout(self.hlayout_lists)
@@ -57,14 +60,15 @@ class SQLDataSearchWidget(QtWidgets.QWidget):
         self.lineEdit_instNum.textChanged.connect(self.list_partids)
         self.lineEdit_partID.textChanged.connect(self.list_partids)
         self.lineEdit_filter.textChanged.connect(self.list_data_files)
-        self.listWidget_PartIDs.itemSelectionChanged.connect(self.list_data_files)
-        self.listWidget_SerialNums.itemSelectionChanged.connect(self.get_file_list)
+        self.listWidget_partIDs.itemSelectionChanged.connect(self.list_data_files)
+        self.listWidget_partIDs.itemSelectionChanged.connect(self.list_serial_numbers)
+        self.listWidget_dataFiles.itemSelectionChanged.connect(self.get_file_list)
 
-        self.partIDsList, self.serialNumsFilesList, self.selected_sns = [], [], []
+        self.partIDsList, self.serialNoList, self.filesList, self.selected_files = [], [], [], []
         self.instrumentIDdict = {}
         
     def list_partids(self):
-        self.listWidget_PartIDs.clear()
+        self.listWidget_partIDs.clear()
         self.partIDsList.clear()
         self.instrumentIDdict.clear()
         display_pIDs = []
@@ -80,40 +84,57 @@ class SQLDataSearchWidget(QtWidgets.QWidget):
                 self.instrumentIDdict[row.Instrument_ID] = [row.Part_ID]
             else:
                 self.instrumentIDdict[row.Instrument_ID].append(row.Part_ID)
-        self.listWidget_PartIDs.addItems(display_pIDs)
+        self.listWidget_partIDs.addItems(display_pIDs)
         mydb.close()
+
+    def list_serial_numbers(self):
+        self.listWidget_serialNums.clear()
+        display_sns = []
+
+        selected_pIDs = [p.row() for p in self.listWidget_partIDs.selectedIndexes()]
+        selected_pIDs = [self.partIDsList[p] for p in selected_pIDs]
+
+        mydb = pyodbc.connect("DRIVER={SQL Server};SERVER=SQLSRV22;DATABASE=SVFLANN;UID=SVUpdater;PWD=MelonBall", readonly=True)
+        mydb_cursor = mydb.cursor()
+
+        for pID in selected_pIDs:
+            for row in mydb_cursor.execute(f"select Serial from [vwSerialHistLookup] where PartId like ?", '%'+pID+'%'):
+                display_sns.append(row.Serial)
+
+        mydb.close()
+        self.listWidget_serialNums.addItems(display_sns)
     
     def list_data_files(self):
-        self.listWidget_SerialNums.clear()
+        self.listWidget_dataFiles.clear()
         display_sn = []
-        self.serialNumsFilesList.clear()
+        self.filesList.clear()
 
-        selected_pIDs = [p.row() for p in self.listWidget_PartIDs.selectedIndexes()]
+        selected_pIDs = [p.row() for p in self.listWidget_partIDs.selectedIndexes()]
         selected_pIDs = [self.partIDsList[p] for p in selected_pIDs]
 
         for dir in selected_pIDs:
             try:
                 list_files = os.listdir(testDataPath+dir)
                 list_files = [name for name in list_files if name.lower().endswith(file_types)]
-                if self.lineEdit_filter.text() != '':
+                if self.lineEdit_filter.text() != '':  # Filter by string
                     filter_list = self.lineEdit_filter.text().split(',')
                     list_files = list(filter(lambda f: any(s in f.lower() for s in filter_list), list_files))
-                self.serialNumsFilesList += [testDataPath+dir+'\\'+s for s in list_files]
+                self.filesList += [testDataPath+dir+'\\'+s for s in list_files]
                 display_sn += list_files
             except:
                 continue
-        self.listWidget_SerialNums.addItems(display_sn)
+        self.listWidget_dataFiles.addItems(display_sn)
 
     def get_selected_files(self):
-        selected_sns = [s.text() for s in self.listWidget_SerialNums.selectedItems()]
-        return selected_sns
+        selected_files = [s.text() for s in self.listWidget_dataFiles.selectedItems()]
+        return selected_files
     
     def get_file_list(self):
-        serialNumsFilesList = self.serialNumsFilesList
+        serialNumsFilesList = self.filesList
         return serialNumsFilesList
     
     def get_selected_instrumentIDs(self):
-        selected_pIDs = [p.row() for p in self.listWidget_PartIDs.selectedIndexes()]
+        selected_pIDs = [p.row() for p in self.listWidget_partIDs.selectedIndexes()]
         selected_pIDs = [self.partIDsList[p] for p in selected_pIDs]
         selected_instrumentIDs = []
         for p in selected_pIDs:
