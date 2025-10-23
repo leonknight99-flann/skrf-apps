7f8fe55c2f1d42fdb708737994e14be4d91a48e7
leonknight99-flann <leon.knight@flann.com>
2025-10-23 14:28:33 +0100

    Change GPIB address

--- skrf_qtapps/skrf_qtwidgets/analyzers/analyzer_zva.py
+++ skrf_qtapps/skrf_qtwidgets/analyzers/analyzer_zva.py
@@ -2,7 +2,7 @@ from skrf.vi.vna.rohde_schwarz import ZVA
 
 
 class Analyzer(ZVA):
-    DEFAULT_VISA_ADDRESS = "GPIB1::20::INSTR"
+    DEFAULT_VISA_ADDRESS = "GPIB0::20::INSTR"
     NAME = "ZVA 50"
     NPORTS = 4
     NCHANNELS = 1
